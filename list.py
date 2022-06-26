9e5738028cb643036052c4c32413b3b6f06ae9d3
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-26 20:35:58 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+# a=[]
+# print(type(a))
+
+p=['kukatpally',23]
+# print(p,type(p))
+# print(p[0],type(p[0]))
+# print(p[1],type(p[1]))
+# print(p[2]) # IndexError: list index out of range
+
+# a='python'
+# print(a,type(a))
+# print(a[0]) #p
+# print(a[-1]) #p
+# print(a[2]) #t
+# print(a[3]) #h
+# print(a[4]) #o
+# print(a[5]) #n
+
+'''Note: modifications cannot be made inside the string data type'''
+# p='visualcode'
+# p[0]='v'
